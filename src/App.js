6e1dcce4c068fd9ed1bf0ede19b6d2ea02b0e492
Sebastian Lugo <sebxstianlugo@gmail.com>
2022-08-29 15:00:00 -0300

    corrección

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import Navbar from './components/Navbar.jsx/Navbar';
 import ItemDetailContainer from './components/ItemDetailContainer.jsx/ItemDetailContainer';
+import ItemListContainer from './components/ItemListContainer.jsx/ItemListContainer.jsx';
+
 
 function App() {
   return (
 <>
  <Navbar/>
+ <ItemListContainer/>
+ <div className='section-1'>
+
+ </div>
  <ItemDetailContainer/>
 
 
